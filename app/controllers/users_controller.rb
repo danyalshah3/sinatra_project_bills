de911ece7366017f87c5e02d7205ef5e6724c30e
MuhammadDanyalMahdi <danyalm50@gmail.com>
2021-06-04 11:03:45 -0400

    removed list of users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,12 @@
 class UsersController < ApplicationController
     
-    get "/users" do
-      @users = User.all
-      erb :"users/index.html"
-    end
 
     # new
     get "/users/new" do
       erb :"users/new.html"
     end
 
-    # users show
-  get "/users/:id" do
-    @user = User.find(params[:id])
-    erb :"users/show.html"
-  end
-
+  
 
     # create
     post "/users" do
