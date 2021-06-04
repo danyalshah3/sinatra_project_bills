62ea05cfca313d1d1d1a574b5f682de80357131f
MuhammadDanyalMahdi <danyalm50@gmail.com>
2021-06-04 10:47:19 -0400

    added users list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,20 @@
 class UsersController < ApplicationController
     
+    get "/users" do
+      @users = User.all
+      erb :"users/index.html"
+    end
 
     # new
     get "/users/new" do
       erb :"users/new.html"
     end
+
+    # users show
+  get "/users/:id" do
+    @user = User.find(params[:id])
+    erb :"users/show.html"
+  end
 
 
     # create
