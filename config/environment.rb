1f075c474067d296ec21d20731373b4e5c7e8ac7
danyalshah3 <72283767+danyalshah3@users.noreply.github.com>
2021-10-17 12:59:50 -0400

    done1

--- config/environment.rb
+++ config/environment.rb
@@ -1,37 +1,38 @@
-# ENV['SINATRA_ENV'] ||= "development"
+ENV['SINATRA_ENV'] ||= "development"
 
-# require 'bundler/setup'
-# Bundler.require(:default, ENV['SINATRA_ENV'])
-# Dotenv.load
+require 'bundler/setup'
+Bundler.require(:default, ENV['SINATRA_ENV'])
+Dotenv.load
 
 # ActiveRecord::Base.establish_connection(
 #   :adapter => "sqlite3",
 #   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
 # )
+set :database_file, './database.yml'
 
-# require './app/controllers/application_controller'
-# require_all 'app'
-require 'bundler/setup'
-Bundler.require
-configure :development do
- ENV['SINATRA_ENV'] ||= "development"
-require 'bundler/setup'
- Bundler.require(:default, ENV['SINATRA_ENV'])
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
- )
-end
+require './app/controllers/application_controller'
+require_all 'app'
+# require 'bundler/setup'
+# Bundler.require
+# configure :development do
+#  ENV['SINATRA_ENV'] ||= "development"
+# require 'bundler/setup'
+#  Bundler.require(:default, ENV['SINATRA_ENV'])
+# ActiveRecord::Base.establish_connection(
+#   :adapter => "sqlite3",
+#   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+#  )
+# end
 
-configure :production do
- db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
+# configure :production do
+#  db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
 
- ActiveRecord::Base.establish_connection(
-   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-   :host     => db.host,
-   :username => db.user,
-   :password => db.password,
-   :database => db.path[1..-1],
-   :encoding => 'utf8'
- )
-end
+#  ActiveRecord::Base.establish_connection(
+#    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+#    :host     => db.host,
+#    :username => db.user,
+#    :password => db.password,
+#    :database => db.path[1..-1],
+#    :encoding => 'utf8'
+#  )
+# end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_05_12_165155) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "expenses", force: :cascade do |t|
     t.date "date"
     t.string "description"
